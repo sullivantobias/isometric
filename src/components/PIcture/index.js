d961ba6aea85b283961159586143f4962f6d7607
sullivan <sullivan.s.tobias-extern@renault.com>
2022-12-14 17:47:23 +0100

    Adding transition to picture scale

--- src/components/PIcture/index.js
+++ src/components/PIcture/index.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
+import { useFrame } from "@react-three/fiber";
 import { Box, useTexture } from "@react-three/drei";
+import { Vector3 } from "three";
 
 const picture = "montage.jpeg";
 
 function Picture({ position }) {
-  const [isHovering, setIsHovering] = useState(false);
   const texture = useTexture(picture);
+  const pictureRef = useRef();
+
+  const [isHovering, setIsHovering] = useState(false);
 
   const handleHovering = (isHovering) => {
     document.body.style.cursor = isHovering ? "pointer" : "auto";
@@ -14,13 +18,19 @@ function Picture({ position }) {
     setIsHovering(isHovering);
   };
 
+  useFrame(() => {
+    const vec = new Vector3(...(isHovering ? [1.2, 1.2, 1.2] : [1, 1, 1]));
+
+    pictureRef.current.scale.lerp(vec, 0.1);
+  });
+
   return (
     <Box
       onPointerOver={() => handleHovering(true)}
       onPointerOut={() => handleHovering(false)}
       position={position}
       args={[0.2, 2, 2]}
-      scale={isHovering ? 1.2 : 1}
+      ref={pictureRef}
     >
       <meshPhongMaterial map={texture} attach="material" />
     </Box>
